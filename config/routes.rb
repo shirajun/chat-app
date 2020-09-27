b434359ea21fb3d38c6486d04c1d29f1e960ac89
shirajun <shirajun3891@icloud.com>
2020-09-27 16:43:25 +0900

    メッセージの送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # ↑↑ルートパスへのアクセスの場合messages_controllerのindexアクションが呼び出されるようにした
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+   resources :messages, only: [:index, :create]
+   #チャットルームに属しているメッセージという意味
+  end
 end
