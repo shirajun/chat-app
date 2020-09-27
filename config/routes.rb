5b612876502e2e1b332f3c7ad6e6faf9cdb9e1be
shirajun <shirajun3891@icloud.com>
2020-09-27 14:51:53 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  #root to: "messages#index"
   # ↑↑ルートパスへのアクセスの場合messages_controllerのindexアクションが呼び出されるようにした
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
